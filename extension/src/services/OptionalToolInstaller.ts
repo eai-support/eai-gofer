acfae3ff9d91bfa988d02af1eea32d790c56f9a2
eai-douglaswross <eai-douglasross@DRM4Pro.local>
2026-09-08 00:41:45 +1000

    fix: handle null PowerShell diagnostics

--- extension/src/services/OptionalToolInstaller.ts
+++ extension/src/services/OptionalToolInstaller.ts
@@ -66,7 +66,7 @@ const PACKAGED_INSTALLERS: Readonly<
   },
   windows: {
     relativePath: ['resources', 'powershell-scripts', 'install-optional-tools.ps1'],
-    sha256: '530c21f8beafcebce88e903a734a4ee7cab8c263a31e2a98a8d5084b8d1abb3f',
+    sha256: 'a7fbfefad761074480f634504fb88d6739050ac95c501dd1d59380e258879811',
   },
 };
 
